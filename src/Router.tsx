89abe38d992efaa5a48dd893701e1935030782b9
Renan Veronez Drechsler <renanvz@outlook.com>
2022-07-15 13:51:51 +0100

    Header

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { History } from './pages/History'
-
 import { Home } from './pages/Home'
 
 export function Router() {
